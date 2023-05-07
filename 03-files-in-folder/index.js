d3be555fa20c9efc72bcc948ebd2a77c6d97de81
vadim-project <vadim.rogozhnikov2002@gmail.com>
2023-05-07 21:13:54 +0300

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const folderPath = path.join(__dirname, 'secret-folder')
+
+fs.readdir(folderPath, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+        fs.stat(path.join(folderPath, file), (err, stats) => {
+            if (err) throw err;
+            const ext = path.extname(file);
+            const name = file.slice(0, -ext.length);
+            let size = stats.size;
+            if (size > 0) console.log(`${name} - ${ext} - ${size}`);
+        });
+    });
+});
+
